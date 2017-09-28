795b99e9c0f2dacf5d0375d6a3c9466bf7e6df12
themeeps <32332666+themeeps@users.noreply.github.com>
2017-09-28 16:18:07 +0700

    LEVEL 1 - External Page Submission

---
+++ form1_external.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Forms</title>
+</head>
+<body>
+
+<form action="form1_process.php" method="post">
+	<input type="text" name="username" placeholder="Enter Username"/>
+	<input type="password" name="password" placeholder="Enter Password"/>
+	<br/>
+	<input type="submit" name="submit" />
+</form>
+
+</body>
+</html>
