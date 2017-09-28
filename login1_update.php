49671bfa7435f9a4e35f321e541cba2a0705df0f
themeeps <32332666+themeeps@users.noreply.github.com>
2017-09-28 23:36:03 +0700

    LEVEL 2 - Query to Read Id’s (part 2)

--- login1_update.php
+++ login1_update.php
@@ -1,14 +1,5 @@
-<?php
-
-include "db.php";
-
-$query = "SELECT * FROM users";
-$result = mysqli_query($koneksi, $query);
-
-if(!$result){
-	die('Query Failed' . mysqli_error($koneksi));
-}
-?>
+<?php include "db.php"; ?>
+<?php include "functions.php"; ?>
 
 <!DOCTYPE html>
 <html>
@@ -32,10 +23,7 @@ if(!$result){
 		<div class="form-group">
 			<select name="id" class="form-control">
 			<?php
-			while($row = mysqli_fetch_assoc($result)){
-				$id = $row['id'];
-				echo "<option value='$id'>$id</option>";
-			}
+			showAllUserId();
 			?>
 			</select>
 		</div>
