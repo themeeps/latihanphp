2d78743182e6c1f2dc02497261c866918203b324
themeeps <32332666+themeeps@users.noreply.github.com>
2017-09-28 15:36:49 +0700

    LEVEL 1 - Extracting Information from Form

--- form1.php
+++ form1.php
@@ -1,6 +1,11 @@
 <?php
 if(isset($_POST['submit'])){
-echo "Form di submit.";
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	
+	echo $username;
+	echo "<br/>";
+	echo $password;
 }
 ?>
 
@@ -11,9 +16,9 @@ echo "Form di submit.";
 </head>
 <body>
 
-<form action="form.php" method="post">
-	<input type="text" placeholder="Enter Username"/>
-	<input type="password" placeholder="Enter Password"/>
+<form action="form1.php" method="post">
+	<input type="text" name="username" placeholder="Enter Username"/>
+	<input type="password" name="password" placeholder="Enter Password"/>
 	<br/>
 	<input type="submit" name="submit" />
 </form>
