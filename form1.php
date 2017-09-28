76c277b50fd4885165f26f1f2a6b17d4dd12c0df
themeeps <32332666+themeeps@users.noreply.github.com>
2017-09-28 16:09:23 +0700

    LEVEL 1 - Validating the Form Values

--- form1.php
+++ form1.php
@@ -1,11 +1,27 @@
 <?php
 if(isset($_POST['submit'])){
+	
+	$name = array("playmaker", "djqllua", "mamikunia", "aoyama");
+	
+	$minimum = 5;
+	$maximum = 10;
+	
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 	
-	echo $username;
-	echo "<br/>";
-	echo $password;
+	if(strlen($username) < $minimum){
+		echo "Username harus memeiliki panjang 5 atau lebih.";
+	}
+	
+	if(strlen($username) > $maximum) {
+		echo "Username tidak boleh lebih panjang dari 10.";
+	}
+	
+	if(!in_array($username, $name)){
+		echo "Maaf, akses ditolak";
+	}else{
+		echo "Selamat datang";
+	}
 }
 ?>
 
