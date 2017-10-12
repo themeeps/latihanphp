c5e2405a0d5f5db8dce718513e4e3fe755609035
themeeps <32332666+themeeps@users.noreply.github.com>
2017-10-12 19:40:07 +0700

    Koneksi

--- Pertemuan-3/koneksi.php
+++ Pertemuan-3/koneksi.php
@@ -7,3 +7,13 @@ if(mysqli_connect_errno()){
 	//jika gagal tampilkan pesan error
 	die(mysqli_connect_error());
 }
+//2. Querry
+$query = "SELECT t_kontak.id,
+				 t_kontak.nama,
+				 t_kontak.hp
+		  FROM t_kontak
+		  INNER JOIN t_kategori
+		  ON t_kontak.kategori = t_kategori.kategori";
+//3. Hasil
+$hasil = mysqli_query($db, $query);
+?>
